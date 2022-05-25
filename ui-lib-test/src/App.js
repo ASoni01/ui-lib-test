5e05ecd9df60c9520f3bd9b535cc908264637e94
Ankita Soni <ankita12soni@gmail.com>
2022-05-25 18:43:51 +0530

    My first commit

---
+++ ui-lib-test/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import {Button} from 'ui-lib';
+
+function App() {
+  return (
+    <div className="App">
+      Hi <Button title="Demo"/>
+    </div>
+  );
+}
+
+export default App;
